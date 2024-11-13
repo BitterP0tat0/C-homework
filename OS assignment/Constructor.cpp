9dabd486232d4a57bc1f54f66d3f3ce27a7149f6
dama123450 <a251080262@gmail.com>
2024-11-13 15:09:54 +0100

    Improve the ouput of taskType

--- OS assignment/Constructor.cpp
+++ OS assignment/Constructor.cpp
@@ -1,16 +1,20 @@
 #include "Constructor.h"
 
 // Default constructor
-Constructor::Constructor() : user(""), taskType(""), taskLength(""), pages(0), arrivalTime(0){}
+Constructor::Constructor() : user(""), taskType(""), taskLength(""), pages(0), arrivalTime(0),taskName("") {}
 
 // initialize task
-void Constructor::initializeTask(const char* user, const char* taskType, const char* taskLength, int pages, int arrivalTime) {
+void Constructor::initializeTask(const char* user, const char* taskType, const char* taskLength, int pages, int arrivalTime,const char* taskName) {
     this->user = user;
     this->taskType = taskType;
     this->taskLength = taskLength;
+    this->taskName = taskName;
     this->pages = pages;
     this->arrivalTime = arrivalTime;
+
 }
+
+
 
 // Getter methods
 const char* Constructor::getUser() const {
@@ -23,6 +27,11 @@ const char* Constructor::getTaskType() const {
 
 const char* Constructor::getTaskLength() const {
     return taskLength.c_str();
+}
+
+
+const char* Constructor::getTaskName() const {
+    return taskName.c_str();
 }
 
 int Constructor::getPages() const {
--- OS assignment/Constructor.h
+++ OS assignment/Constructor.h
@@ -7,21 +7,26 @@ private:
     std::string user;         // User for the task
     std::string taskType;     // Type of task (Print or Scan)
     std::string taskLength;   // Length of task (Short, Medium, Long)
+    std::string taskName;     // Nmae of the tasks 
     int pages;                // Number of pages for the task
     int arrivalTime;          // Arrival time for the task
 
 public:
-    // Default constructor
     Constructor();
 
-    // Method to initialize task
-    void initializeTask(const char* user, const char* taskType, const char* taskLength, int pages, int arrivalTime);
+    void initializeTask(const char* user, const char* taskType, const char* taskLength, int pages, int arrivalTime, const char* taskName);
+
+ 
 
 
     // Getter methods
     const char* getUser() const;
     const char* getTaskType() const;
+    const char* getTaskName() const;
     const char* getTaskLength() const;
     int getPages() const;
     int getArrivalTime() const;
+
+   
+
 };
